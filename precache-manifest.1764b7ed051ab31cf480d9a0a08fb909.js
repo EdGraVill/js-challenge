8db0f59ee8b3d555052f74d67c807f489a6c9a18
Eduardo Grajales Villanueva <edgravill@gmail.com>
2020-01-15 16:17:11 -0600

    Updates

---
+++ precache-manifest.1764b7ed051ab31cf480d9a0a08fb909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3d5175a1555176815feebeb973bf2e",
+    "url": "/js-challenge/index.html"
+  },
+  {
+    "revision": "28b2019873b1061d98bf",
+    "url": "/js-challenge/static/js/2.9595e3fd.chunk.js"
+  },
+  {
+    "revision": "af2b0c0521f997c41289419609de4522",
+    "url": "/js-challenge/static/js/2.9595e3fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb8f03ed8dc3ac1fde5d",
+    "url": "/js-challenge/static/js/3.919a2329.chunk.js"
+  },
+  {
+    "revision": "f28fbf17edab86adbb44",
+    "url": "/js-challenge/static/js/main.1110d900.chunk.js"
+  },
+  {
+    "revision": "8b9f0861a58220ff454d",
+    "url": "/js-challenge/static/js/runtime-main.6b1fec76.js"
+  }
+]);
